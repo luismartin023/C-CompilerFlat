c961d7579a983c1607798e6a34bc15d0c6a07f91
Luis Martin Peña Mejia <luismartinpm2020@gmail.com>
2026-09-12 21:02:53 -0400

    INTERFAZ: OCULTAR CONSOLA DEL LANZADOR

--- CCompilerFlat.bat
+++ CCompilerFlat.bat
@@ -1,4 +1,4 @@
 @echo off
 title CCompilerFlat - Instalador C/C++
-powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0src\CCompilerFlat.ps1"
+powershell.exe -NoProfile -WindowStyle Hidden -ExecutionPolicy Bypass -File "%~dp0src\CCompilerFlat.ps1"
 if errorlevel 1 pause
